f8d5347977e3c0b0559fcb2d757e8d71eb0e9822
Sam Vaughton <samvaughton@gmail.com>
2019-05-16 13:53:35 +0100

    Discounts also supported

--- src/Los/Modifier/ModifierExtractor.php
+++ src/Los/Modifier/ModifierExtractor.php
@@ -30,6 +30,10 @@ class ModifierExtractor
                 return true; // Any hidden ones are supported
             }
 
+            if ($modifier->getRate()->getCalculationOperand() === 'subtraction') {
+                return true; // Discounts always supported since it will be part of base price
+            }
+
             if (empty($modifier->getConditions())) {
                 return false; // Mandatory, non-hidden modifiers are NOT supported
             }
