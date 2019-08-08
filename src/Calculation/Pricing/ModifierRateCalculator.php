274cd1ab003920bc01c06a483e61dbcaaa984de5
Sam Vaughton <samvaughton@gmail.com>
2019-08-08 12:58:34 +0100

    Removed unused code

--- src/Calculation/Pricing/ModifierRateCalculator.php
+++ src/Calculation/Pricing/ModifierRateCalculator.php
@@ -3,8 +3,6 @@
 namespace Aptenex\Upp\Calculation\Pricing;
 
 use Aptenex\Upp\Context\PricingContext;
-use Aptenex\Upp\Util\MoneyUtils;
-use Aptenex\Upp\Helper\MoneyTools;
 use Aptenex\Upp\Calculation\FinalPrice;
 
 class ModifierRateCalculator
@@ -17,36 +15,8 @@ class ModifierRateCalculator
     public function compute(PricingContext $context, FinalPrice $fp)
     {
         foreach($fp->getStay()->getModifiersUsed() as $modifier) {
-            if (false) {
-                $rateConfig = $modifier->getControlItemConfig()->getRate();
-
-                $daysMatched = count($modifier->getMatchedNights());
-
-                if ($rateConfig->getAmount() === 0 || $daysMatched === 0) {
-                    continue;
-                }
-
-                if ($rateConfig->getType() === \Aptenex\Upp\Parser\Structure\Rate::TYPE_ADJUSTMENT) {
-
-                    foreach ($modifier->getMatchedNights() as $key => $day) {
-                        if ($rateConfig->getCalculationMethod() === \Aptenex\Upp\Parser\Structure\Rate::METHOD_PERCENTAGE) {
-                            $value = $day->getCost()->multiply($rateConfig->getAmount());
-                        } else {
-                            $value = MoneyUtils::fromString($rateConfig->getAmount(), $fp->getCurrency());
-                        }
-
-                        $day->setCost(MoneyTools::applyMonetaryOperand(
-                            $day->getCost(),
-                            $value,
-                            $rateConfig->getCalculationOperand()
-                        ));
-                    }
-
-                }
-            } else {
-                $cuc = new RatePerConditionalUnitCalculator();
-                $cuc->applyConditionalRateModifications($fp, $modifier);
-            }
+            $cuc = new RatePerConditionalUnitCalculator();
+            $cuc->applyConditionalRateModifications($fp, $modifier);
         }
     }
 
